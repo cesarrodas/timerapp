a0f47ef6474d23703f7af876716b430d0f41b9ec
cesar <rodascesar03@yahoo.com>
2022-10-01 13:58:47 -0500

    attempting to fix path

--- timerapp.js
+++ timerapp.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = new express();
+const path = require('path');
 const port = 7000;
 
-app.use(express.static("./dist"));
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
